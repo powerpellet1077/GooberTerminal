4cfddadfce7cd63353fa87962b8d922043d311ef
powerpellet1077 <70673296+powerpellet1077@users.noreply.github.com>
2025-05-01 16:29:47 -0700

    Update latest.bat

--- latest.bat
+++ latest.bat
@@ -1,4 +1,4 @@
 cd /D %HOMEDRIVE%%HOMEPATH%
-curl -L -o goob.exe https://github.com/powerpellet1077/GooberTerminal/raw/refs/heads/main/goobs/goob043025_7.exe
+curl -L -o goob.exe https://github.com/powerpellet1077/GooberTerminal/raw/refs/heads/main/goobs/goob050125_1.exe
 start goob.exe
 exit
