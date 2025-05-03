d89f12d9be6f4da18297f8d55d8327fb47c7cce8
powerpellet1077 <70673296+powerpellet1077@users.noreply.github.com>
2025-05-03 16:04:42 -0700

    Update latest.ps1

--- latest.ps1
+++ latest.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest -Uri "https://github.com/powerpellet1077/GooberTerminal/raw/refs/heads/main/goobs/goob050225_3.exe" -OutFile "goob.exe"
+Invoke-WebRequest -Uri "https://github.com/powerpellet1077/GooberTerminal/raw/refs/heads/main/goobs/goob050325_1.exe" -OutFile "goob.exe"
 Start-Process -FilePath "goob.exe"
 exit
